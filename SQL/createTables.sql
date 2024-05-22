ce95257288be7da069dd2a5ccb3e09a4d13be19e
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-05-22 11:33:53 -0500

    Modified SQL scripts to better represent the business model.

--- SQL/createTables.sql
+++ SQL/createTables.sql
@@ -8,7 +8,8 @@ CREATE TABLE users (
 );
 
 CREATE TABLE mainpage (
-
+    id varchar(255) NOT NULL PRIMARY KEY,
+    user_id varchar(255) NOT NULL foreign KEY (user_id) references users (id)
     url VARCHAR(255) NOT NULL,
     user VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
