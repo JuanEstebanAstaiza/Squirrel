979402fcf87346b53ca335bc94a3068cb6334184
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-05-22 11:37:23 -0500

    Changed the Squire model-type to better represent the business model

--- Models/squire_model.go
+++ Models/squire_model.go
@@ -1,6 +1,8 @@
 package Models
 
 type Squire struct {
+	ID       string `json:"id"`
+	UserId   string `json:"user_id"`
 	Url      string `json:"url"`
 	Password string `json:"password"`
 	Username string `json:"username"`
